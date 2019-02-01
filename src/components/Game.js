ad6645eb90714a05cef40c2f71abfc314c54eb8e
Corey Ladovsky <coreyskymusic@gmail.com>
2019-02-01 12:50:00 -0500

    making board title objects

---
+++ src/components/Game.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class Game extends React.Component {
+  state = { grid: [] }
+
+  componentDidMount() {
+    this.newGrid();
+  }
+
+  newGrid() {
+    let grid = [];
+    for(let i = 0; i < 12; i++) {
+      let row = []
+      for(let j = 0; j < 12; j++) {
+        if(!j && !i) {
+          row.push(this.createObj(true, [i, j]));
+        } else {
+          row.push(this.createObj(false, [i, j]));
+        }
+      }
+      grid.push(row)
+    }
+    this.setState({ grid })
+  }
+
+  createObj(active, location) {
+    let colors = ["blue", "red", "yellow", "green", "orange"]
+    let color = colors[Math.floor(Math.random() * colors.length)]
+    return { color, active, location, id: location.toString() }
+  }
+
+  render() {
+    return(
+      <div className="board">
+
+      </div>
+
+    )
+  }
+
+
+}
